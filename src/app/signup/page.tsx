bdb3bb36ddb2228364ecac739495af914ecff182
kodai9263 <kou92638685@gmail.com>
2025-10-21 13:57:31 +0900

    fix reset-password signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { supabase } from "@/src/utils/supabase";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Page() {
