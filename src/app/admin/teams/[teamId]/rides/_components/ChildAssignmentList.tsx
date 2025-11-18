1510033ccf64423fd92999adf34b5d5eb5080dbe
kodai9263 <kou92638685@gmail.com>
2025-11-18 16:24:05 +0900

    feat DriverHeader ChildAssignmentList

---
+++ src/app/admin/teams/[teamId]/rides/_components/ChildAssignmentList.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { UpdateRideValues } from "@/app/_types/ride";
+import { Minus, Plus, User } from "lucide-react";
+import { Control, useFieldArray, UseFormRegister } from "react-hook-form";
+
+interface Props {
+  index: number;
+  control: Control<UpdateRideValues>;
+  register: UseFormRegister<UpdateRideValues>;
+  childrenList: { id: number; name: string }[];
+};
+
+export default function ChildAssignmentList({
+  index, control, register, childrenList,
+}: Props) {
+  const { fields, append, remove, } = useFieldArray({
+    control,
+    name: `drivers.${index}.rideAssignments`,
+  });
+
+  return (
+    <div>
+      {fields.map((item, childIndex) =>(
+        <div key={item.id} className="">
+          <User size={18} />
+
+          <select 
+            {...register(`drivers.${index}.rideAssignments.${childIndex}.childId`,
+              { required: true, valueAsNumber: true }
+            )}
+            className=""
+          >
+            <option value="">選択</option>
+            {childrenList.map((child) => (
+              <option key={child.id} value={child.id}>
+                {child.name}
+              </option>
+            ))}
+          </select>
+
+          <button
+            type="button"
+            onClick={() => remove(childIndex)}
+            className=""
+          >
+            <Minus size={20} />
+          </button>
+        </div>
+      ))}
+
+      <button
+        type="button"
+        onClick={() => append({ childId: 0 })}
+        className=""
+      >
+        <Plus size={20} />
+      </button>
+    </div>
+  );
+}
---
+++ src/app/admin/teams/[teamId]/rides/_components/DriverHeader.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { UpdateRideValues } from "@/app/_types/ride";
+import { CarFront, X } from "lucide-react";
+import { UseFormRegister } from "react-hook-form";
+
+interface Props {
+  index: number;
+  register: UseFormRegister<UpdateRideValues>;
+  availabilityDrivers: {
+    id: number;
+    member: { id: number; name: string };
+  }[];
+  onRemove: () => void;
+};
+
+export default function DriverHeader({
+  index,
+  register,
+  availabilityDrivers,
+  onRemove,
+}: Props) {
+  return (
+    <div className="">
+      {/* 配車号削除ボタン */}
+      <button
+        type="button"
+        onClick={onRemove}
+        className=""
+      >
+        <X size={20} />
+      </button>
+
+      {/* 配車号 */}
+      <div className="">
+        <CarFront size={22} />
+        <select 
+          {...register(`drivers.${index}.availabilityDriverId`, {
+            required: true,
+            valueAsNumber: true,
+          })}
+          className=""
+        >
+          <option value="">選択</option>
+          {availabilityDrivers.map((driver) => (
+            <option key={driver.id} value={driver.id}>
+              {driver.member.name}号
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
